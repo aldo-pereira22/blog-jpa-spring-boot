67db55635f8f091b6bfad812e8f70105fb6ea2e9
Aldo Pereira <aldopereira.gyn@gmail.com>
2021-05-15 13:28:05 -0300

    Salvando Posts

--- src/main/java/com/dev22/blogjpaspringboot/controller/CodeBlogController.java
+++ src/main/java/com/dev22/blogjpaspringboot/controller/CodeBlogController.java
@@ -1,12 +1,17 @@
 package com.dev22.blogjpaspringboot.controller;
 
+import java.time.LocalDate;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.dev22.blogjpaspringboot.model.Post;
 import com.dev22.blogjpaspringboot.service.CodeBlogService;
@@ -24,5 +29,50 @@ public class CodeBlogController {
 		mv.addObject("posts", posts);
 		return mv;		
 	}
+
+	@RequestMapping(value = "/posts/{id}", method = RequestMethod.GET)
+	public ModelAndView getPostDetails( @PathVariable("id") Long id ) {
+		ModelAndView mv =  new ModelAndView("postDetails ");
+		Post post = codBlogService.findById(id);
+		mv.addObject("post", post);
+		return mv;		
+	}
+	
+	@RequestMapping(value = "/newpost", method = RequestMethod.GET)
+	public String getPostForm() {
+		return "postForm";
+	}
+	@RequestMapping(value = "/newpost", method = RequestMethod.POST)
+	public String savePost(@Validated Post post, BindingResult result, RedirectAttributes atributes) {
+		
+		if(result.hasErrors()) {
+			return "redirect:/newpost";
+		}
+		post.setData(LocalDate.now());
+		
+		codBlogService.save(post);
+		return "redirect:/posts";
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 }
